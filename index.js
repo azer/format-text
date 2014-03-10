b1f4e50959c426433fc59752c95f21eca4990bd7
E.Azer Koçulu <azer@kodfabrik.com>
2014-03-10 15:46:26 -0700

    clean

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
 module.exports = format;
 
-function findContext(args){
-  if(typeof args[1] == 'object' && args[1])
-    return args[1];
-
-  return Array.prototype.slice.call(args, 1);
-}
-
 function format(text) {
-  var context = findContext(arguments);
+  var context;
+
+  if (typeof arguments[1] == 'object' && arguments[1]) {
+    context = arguments[1];
+  } else {
+    context = Array.prototype.slice.call(arguments, 1);
+  }
 
   return String(text).replace(/\{?\{([^{}]+)}}?/g, replace(context));
 };
 
-function replace(context, nil){
-
-  return function(tag, name) {
-
-    if(tag.substring(0, 2) == '{{' && tag.substring(tag.length - 2) == '}}'){
+function replace (context, nil){
+  return function (tag, name) {
+    if (tag.substring(0, 2) == '{{' && tag.substring(tag.length - 2) == '}}') {
       return '{' + name + '}';
     }
 
-    if( !context.hasOwnProperty(name) ){
+    if (!context.hasOwnProperty(name)) {
       return tag;
     }
 
-    if( typeof context[name] == 'function' ){
+    if (typeof context[name] == 'function') {
       return context[name]();
     }
 
     return context[name];
-
   }
-
 }
